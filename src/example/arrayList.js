5624124f36a327eeb24277369396ce9b0d09a6e8
liuhuihao <1061036802@qq.com>
2020-01-13 15:11:50 +0800

    feat: sort

--- src/example/arrayList.js
+++ src/example/arrayList.js
@@ -1,19 +1,19 @@
 import { ArrayList } from '../structure'
 
 const a = new ArrayList()
-for (let i = 0; i <= 10000; i++) {
-  a.insert(parseInt((Math.random() * 100 + '')))
-}
-// a.insert(1)
-// a.insert(9)
-// a.insert(7)
-// a.insert(3)
-// a.insert(6)
-// a.insert(2)
+// for (let i = 0; i <= 10; i++) {
+//   a.insert(parseInt((Math.random() * 100 + '')))
+// }
+a.insert(9)
+a.insert(1)
+a.insert(4)
+a.insert(2)
+a.insert(3)
+a.insert(6)
 
 const time = new Date().getTime()
-a.insertionSort()
+a.quickSort()
 console.log(a)
-console.log(new Date().getTime() - time)
+// console.log(new Date().getTime() - time)
 
 
--- src/structure/arrayList.js
+++ src/structure/arrayList.js
@@ -41,6 +41,79 @@ class ArrayList {
     }
   }
 
+  // 归并排序 - 将数组拆成单项后再组合排序
+  mergeSort () {
+    this.array = this._mergeSortRec(this.array)
+  }
+
+  // 快速排序
+  quickSort () {
+    this._quick(this.array, 0, this.array.length - 1)
+  }
+
+  _quick (arr, left, right) {
+    if (arr.length > 1) {
+      const index = this._partition(arr, left, right) // 主元下标 index
+      if (left < index - 1) {
+        this._quick(arr, left, index - 1)
+      }
+      if (index < right) {
+        this._quick(arr, index, right)
+      }
+    }
+  }
+
+  _partition (arr, left, right) {
+    let pivot = arr[Math.floor((left + right) / 2)], l = left, r = right
+    while (l <= r) {
+      while (arr[l] < pivot) {
+        l++
+      }
+      while (arr[r] > pivot) {
+        r--
+      }
+      if (l <= r) {
+        this._swap(l, r)
+        l++
+        r--
+      }
+    }
+    return l
+  }
+
+  _mergeSortRec (arr) {
+    if (arr.length === 1) {
+      return arr
+    } else {
+      const mid = Math.round(arr.length / 2)
+      const leftArr = arr.slice(0, mid)
+      const rightArr = arr.slice(mid, arr.length)
+      return this._merge(this._mergeSortRec(leftArr), this._mergeSortRec(rightArr))
+    }
+  }
+
+  _merge (left, right) {
+    let re = [], il = 0, ir = 0
+    while (il < left.length && ir < right.length) {
+      if (left[il] < right[ir]) {
+        re.push(left[il])
+        il++
+      } else {
+        re.push(right[ir])
+        ir++
+      }
+    }
+    while (il >= left.length && ir < right.length) {
+      re.push(right[ir])
+      ir++
+    }
+    while (ir >= right.length && il < left.length) {
+      re.push(left[il])
+      il++
+    }
+    return re
+  }
+
   _swap (index1, index2) {
     const temp = this.array[index1]
     this.array[index1] = this.array[index2]
